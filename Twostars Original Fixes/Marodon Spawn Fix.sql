fec563f2ebe984af04057046af69afa0887b0e69
can <62135319+cansnoxd@users.noreply.github.com>
2020-05-19 04:18:47 +0300

    Add files via upload

---
+++ Twostars Original Fixes/Marodon Spawn Fix.sql
@@ -0,0 +1,36 @@
+USE [KN_online]
+GO
+/****** Object:  StoredProcedure [dbo].[LOAD_USER_DATA]    Script Date: 19.5.2020 04:16:01 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER OFF
+GO
+
+/****** Object:  Stored Procedure dbo.LOAD_USER_DATA    Script Date: 6/6/2006 6:03:33 PM ******/
+
+/****** 개체: 저장 프로시저 dbo.LOAD_USER_DATA    스크립트 날짜: 2002-11-14 오전 11:18:04 ******/
+-- modifed by Sungyong 2003.04.09
+-- 2004.04.22 월간기여도(LoyaltyMonthly) 추가 arycoat
+
+ALTER PROCEDURE [dbo].[LOAD_USER_DATA]
+    @AccountID  char(21) ,
+    @id             char(21),
+    @nRet        smallint OUTPUT
+AS
+BEGIN
+    DECLARE @PosX int, @PosZ int, @PosY int, @Zone tinyint
+    SELECT @Zone = Zone, @PosX = px, @PosZ = pz, @PosY = py FROM USERDATA WHERE strUserId = @id
+
+    -- If the zone is Moradon...
+    IF @Zone = 21
+    BEGIN
+        -- Use old Moradon co-ordinates ( * 100)
+        -- I'm not using the new map but you can change the co-ordinates here.
+        SET @PosX = 31100
+        SET @PosY = 0 -- If we're higher or lower (depth-wise), we set this. Otherwise, it remains 0.
+        SET @PosZ = 35100
+    END
+
+    SELECT Nation, Race, Class, HairColor, Rank, Title, [Level], [Exp], Loyalty, Face, City, Knights, Fame, Hp, Mp, Sp, Strong, Sta, Dex, Intel, Cha, Authority, Points, Gold, [Zone], Bind, @PosX, @PosZ, @PosY, dwTime, strSkill, strItem,strSerial, sQuestCount, strQuest, MannerPoint, LoyaltyMonthly FROM    USERDATA WHERE strUserId = @id
+    SET @nRet = @@RowCount
+END
---
+++ Twostars Original Fixes/USER_MANNERPOINT_RANK.sql
@@ -0,0 +1,30 @@
+USE [KN_online]
+GO
+
+/****** Object:  Table [dbo].[USER_MANNERPOINT_RANK]    Script Date: 11.4.2020 00:04:08 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[USER_MANNERPOINT_RANK](
+	[nRank] [smallint] NOT NULL,
+	[nElmoUP] [smallint] NOT NULL,
+	[strElmoUserID] [char](21) NULL,
+	[nElmoMannerPoint] [int] NULL,
+	[nKarusUP] [smallint] NOT NULL,
+	[strKarusUserID] [char](21) NULL,
+	[nKarusMannerPoint] [int] NULL,
+	[UpdateDate] [smalldatetime] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
